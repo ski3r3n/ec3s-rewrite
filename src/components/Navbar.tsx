64d05024a3db79986ec9b7de65fdd23d3c24b975
Canaris <mrgeek484@gmail.com>
2024-10-09 18:51:32 +0800

    Refactor MobileMenu component to use Headless UI; remove Radix UI dependency and update animation handling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+// import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useState } from 'react';
+// import { useState } from 'react';
+import { Fragment } from 'react';
 
 const menuLinks = [
 	{ title: <span>About</span>, href: '/#about' },
@@ -17,52 +19,103 @@ const menuLinks = [
 ];
 
 function MobileMenu() {
-	const [open, setOpen] = useState(false);
-
 	return (
-		<DropdownMenu.Root open={open} onOpenChange={setOpen}>
-			<DropdownMenu.Trigger asChild>
-				<motion.button
-					className='px-3 cursor-pointer outline-none active:outline-none'
-					whileHover={{ scale: 1.1 }}
-					whileTap={{ scale: 0.9 }}
-				>
-					<span className='icon-[mdi--menu] text-xl'></span>
-				</motion.button>
-			</DropdownMenu.Trigger>
+		<Menu>
+			{({ open }) => (
+				<>
+					<MenuButton
+						as={motion.button}
+						className='px-3 cursor-pointer outline-none active:outline-none'
+						whileHover={{ scale: 1.1 }}
+						whileTap={{ scale: 0.9 }}
+					>
+						<span className='icon-[mdi--menu] text-xl'></span>
+					</MenuButton>
 
-			<DropdownMenu.Content sideOffset={5} alignOffset={0} asChild>
-				<AnimatePresence>
-					{open && (
-						<motion.div
-							className='mr-5'
-							initial={{ opacity: 0, x: 20, y: -40, scale: 0.1 }}
-							animate={{ opacity: 1, x: 0, y: 0, scale: 1 }}
-							exit={{ opacity: 0, x: 20, y: -40, scale: 0.1 }}
-							transition={{
-								duration: 0.2,
-								type: 'spring',
-								damping: 20,
-								stiffness: 200,
-							}}
-						>
-							{menuLinks.map((link, i) => (
-								<a
-									key={i}
-									href={link.href}
-									className='block px-3 py-2'
-									target={link.href.startsWith('http') ? '_blank' : ''}
+					<AnimatePresence>
+						{open && (
+							<MenuItems static as={Fragment}>
+								<motion.div
+									initial={{ opacity: 0, x: 20, y: -40, scale: 0.1 }}
+									animate={{ opacity: 1, x: 0, y: 0, scale: 1 }}
+									exit={{ opacity: 0, x: 20, y: -40, scale: 0.1 }}
+									transition={{
+										duration: 0.2,
+										type: 'spring',
+										damping: 20,
+										stiffness: 200,
+									}}
+									className='outline-none active:outline-none focus:outline-none'
 								>
-									{link.title}
-								</a>
-							))}
-						</motion.div>
-					)}
-				</AnimatePresence>
-			</DropdownMenu.Content>
-		</DropdownMenu.Root>
+									{menuLinks.map((link, i) => (
+										<MenuItem key={i}>
+											{() => (
+												<a
+													href={link.href}
+													className={`block px-3 py-2`}
+													target={link.href.startsWith('http') ? '_blank' : ''}
+												>
+													{link.title}
+												</a>
+											)}
+										</MenuItem>
+									))}
+								</motion.div>
+							</MenuItems>
+						)}
+					</AnimatePresence>
+				</>
+			)}
+		</Menu>
 	);
 }
+// function MobileMenu() {
+// 	const [open, setOpen] = useState(false);
+
+// 	return (
+// 		<DropdownMenu.Root open={open} onOpenChange={setOpen}>
+// 			<DropdownMenu.Trigger asChild>
+// 				<motion.button
+// 					className='px-3 cursor-pointer outline-none active:outline-none'
+// 					whileHover={{ scale: 1.1 }}
+// 					whileTap={{ scale: 0.9 }}
+// 				>
+// 					<span className='icon-[mdi--menu] text-xl'></span>
+// 				</motion.button>
+// 			</DropdownMenu.Trigger>
+
+// 			<DropdownMenu.Content sideOffset={5} alignOffset={0} asChild>
+// 				<AnimatePresence>
+// 					{open && (
+// 						<motion.div
+// 							className='mr-5 duration-200'
+// 							initial={{ opacity: 0, x: 20, y: -40, scale: 0.1 }}
+// 							animate={{ opacity: 1, x: 0, y: 0, scale: 1 }}
+// 							exit={{ opacity: 0, x: 20, y: -40, scale: 0.1 }}
+// 							// transition={{
+// 							// 	duration: 0.2,
+// 							// 	type: 'spring',
+// 							// 	damping: 20,
+// 							// 	stiffness: 200,
+// 							// }}
+// 						>
+// 							{menuLinks.map((link, i) => (
+// 								<a
+// 									key={i}
+// 									href={link.href}
+// 									className='block px-3 py-2'
+// 									target={link.href.startsWith('http') ? '_blank' : ''}
+// 								>
+// 									{link.title}
+// 								</a>
+// 							))}
+// 						</motion.div>
+// 					)}
+// 				</AnimatePresence>
+// 			</DropdownMenu.Content>
+// 		</DropdownMenu.Root>
+// 	);
+// }
 
 export default function Navbar() {
 	return (
