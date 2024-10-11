ca180d9e50804dcf8907c0407cf7ad1d2a6eb1ff
Canaris <mrgeek484@gmail.com>
2024-10-11 13:22:26 +0800

    Refactor MobileMenu component for improved accessibility and animation; update Navbar responsive classes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,52 +20,63 @@ const menuLinks = [
 function MobileMenu() {
 	return (
 		<Menu>
-			{({ open }) => (
-				<>
-					<MenuButton
-						as={motion.button}
-						className='px-3 cursor-pointer outline-none active:outline-none'
-						whileHover={{ scale: 1.1 }}
-						whileTap={{ scale: 0.9 }}
-					>
-						<span className='icon-[mdi--menu] text-xl'></span>
-					</MenuButton>
+			{({ open }) => {
+				document.documentElement.style.overflow = 'auto';
 
-					<AnimatePresence>
-						{open && (
-							<MenuItems static as={Fragment}>
-								<motion.div
-									layout
-									initial={{ opacity: 0, x: 20, y: -40, scale: 0.1 }}
-									animate={{ opacity: 1, x: 0, y: 0, scale: 1 }}
-									exit={{ opacity: 0, x: 20, y: -40, scale: 0.1 }}
-									transition={{
-										duration: 0.2,
-										type: 'spring',
-										damping: 20,
-										stiffness: 120,
-									}}
-									className='outline-none active:outline-none focus:outline-none'
-								>
-									{menuLinks.map((link, i) => (
-										<MenuItem key={i}>
-											{() => (
-												<a
-													href={link.href}
-													className={`block px-3 py-2`}
-													target={link.href.startsWith('http') ? '_blank' : ''}
-												>
-													{link.title}
-												</a>
-											)}
-										</MenuItem>
-									))}
-								</motion.div>
-							</MenuItems>
-						)}
-					</AnimatePresence>
-				</>
-			)}
+				return (
+					<>
+						<MenuButton
+							as={motion.button}
+							className='px-3 cursor-pointer outline-none active:outline-none'
+							whileHover={{ scale: 1.1 }}
+							whileTap={{ scale: 0.9 }}
+						>
+							{({ open }) => (
+								<span
+									className={`icon-[mdi--${open ? 'close' : 'menu'}] text-xl transition-transform duration-300`}
+									style={{ transform: open ? 'rotate(90deg)' : 'rotate(0deg)' }}
+								></span>
+							)}
+						</MenuButton>
+
+						<AnimatePresence>
+							{open && (
+								<MenuItems static as={Fragment}>
+									<motion.div
+										layout
+										initial={{ opacity: 0, x: 20, y: -40, scale: 0.1 }}
+										animate={{ opacity: 1, x: 0, y: 0, scale: 1 }}
+										exit={{ opacity: 0, x: 20, y: -40, scale: 0.1 }}
+										transition={{
+											duration: 0.2,
+											type: 'spring',
+											damping: 20,
+											stiffness: 120,
+										}}
+										className='outline-none active:outline-none focus:outline-none'
+									>
+										{menuLinks.map((link, i) => (
+											<MenuItem key={i}>
+												{() => (
+													<a
+														href={link.href}
+														className={`block px-3 py-2`}
+														target={
+															link.href.startsWith('http') ? '_blank' : ''
+														}
+													>
+														{link.title}
+													</a>
+												)}
+											</MenuItem>
+										))}
+									</motion.div>
+								</MenuItems>
+							)}
+						</AnimatePresence>
+					</>
+				);
+			}}
 		</Menu>
 	);
 }
@@ -131,7 +142,7 @@ export function Navbar() {
 					className='w-10 inline-block float-left'
 				/>
 			</a>
-			<div className='sm:inline-block hidden float-right mt-2'>
+			<div className='md:inline-block hidden float-right mt-2'>
 				{menuLinks.map((link, i) => (
 					<a
 						key={i}
@@ -143,7 +154,7 @@ export function Navbar() {
 					</a>
 				))}
 			</div>
-			<div className='sm:hidden inline-block float-right mt-2'>
+			<div className='md:hidden inline-block float-right mt-2 pr-0'>
 				<MobileMenu />
 			</div>
 		</div>
